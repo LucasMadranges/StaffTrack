2ef1b63865b79a775e01e8673f88de9cce97e93f
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-09-14 12:08:12 +0200

    fix: api work on port 3000 and not 4000 has it wanted to fix: angular not working on port 4200

--- AppWeb/server.ts
+++ AppWeb/server.ts
@@ -1,9 +1,9 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
-import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
-import bootstrap from './src/main.server';
+import {APP_BASE_HREF} from "@angular/common";
+import {CommonEngine} from "@angular/ssr";
+import express from "express";
+import {fileURLToPath} from "node:url";
+import {dirname, join, resolve} from "node:path";
+import bootstrap from "./src/main.server";
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -44,7 +44,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env["PORT"] || 5000;
 
   // Start up the Node server
   const server = app();
